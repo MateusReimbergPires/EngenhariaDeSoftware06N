eab0b52eeb282f9349a2aa382c005c9622fb829f
Guilherme <gui.leme01@outlook.com>
2024-05-24 19:13:33 -0300

    Add files via upload

--- tests/test_vaga.py
+++ tests/test_vaga.py
@@ -2,6 +2,6 @@ from app.models import *
 
 def test_vaga():
     vaga = Vaga(nomeVaga = 'Teste', departamento_id = 1, areaAtuacao_id = 1)
-    assert vaga.nomevaga == 'Teste'
+    assert vaga.nomeVaga == 'Teste'
     perfilDesejado = PerfilDesejado(vaga_id=vaga.id)
     assert perfilDesejado.vaga_id == vaga.id
